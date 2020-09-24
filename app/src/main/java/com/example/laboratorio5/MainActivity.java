f5817cca4b27524ba4bfbc3d06ff7a219bf45e2b
Erika <erikanoemychuya@gmail.com>
2020-09-24 15:27:51 -0400

    ultimos cambios

--- app/src/main/java/com/example/laboratorio5/MainActivity.java
+++ app/src/main/java/com/example/laboratorio5/MainActivity.java
@@ -13,11 +13,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ArrayAdapter<String> adapter;
     private ArrayList<String> datos;
@@ -47,6 +50,35 @@ public class MainActivity extends AppCompatActivity {
     public void LoadComponents(){
         GridView container= (GridView) this.findViewById(R.id.container);
         container.setAdapter(adapter);
+
+        Button add= this.findViewById(R.id.add);
+        add.setOnClickListener(this);
+        Button remove=this.findViewById(R.id.remove);
+        remove.setOnClickListener(this);
+        Button roll = this.findViewById(R.id.roll);
+        roll.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.add){
+            datos.add("1");
+        }else if(v.getId()== R.id.remove){
+            datos.remove(datos.size() -1); // aaaaaaaaa
+        }else if (v.getId() == R.id.roll){
+            int size = datos.size();
+            datos.clear();
+            int suma=0;
+            for (int i=0 ; i< size ; i++){
+                int number = ThreadLocalRandom.current().nextInt(1,6);
+                suma += number;
+                datos.add(number + "");
+            }
+            TextView txt = (TextView) this.findViewById(R.id.results);
+            txt.setText("El total es " +suma);
+        }
+        adapter.clear();
+        adapter.addAll(datos);
     }
 
     @Override
